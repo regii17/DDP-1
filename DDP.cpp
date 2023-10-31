90f8a11e6d786f8bb90e5dbc1004a9995f19c837
Regiana Hermawan <108383190+regii17@users.noreply.github.com>
2023-11-01 01:06:37 +0700

    Update DDP.cpp

--- DDP.cpp
+++ DDP.cpp
@@ -6,29 +6,24 @@ int main() {
 	
 
 		int b=1;
-		for(b=1;b<=7;b++){
+	for(b=1;b<=7;b++){
 		if (b%2==0){
-				cout<<"__";
+			cout<<"__";
 				
-			}
-			else{
+		}else{
 			cout<<" ";
 		}
 		}
-		cout<<endl;	
-	for(int d=1;d<=3;d++){
+		cout<<endl;
 	
+	for(int d=1;d<=3;d++){
 		int c=1;
-		for(c=1;c<=7;c++){
-			
-			if (c%2==0){
-				cout<<"__";
-				
-			}
-			else{
+	for(c=1;c<=7;c++){
+		if (c%2==0){
+			cout<<"__";
+		}else{
 			cout<<"|";
 		}
-			
 		}
 		cout<<endl;	
 	
