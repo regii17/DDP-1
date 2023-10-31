bf761ff2c677521316c79972859b7ccaa6b6f844
Regiana Hermawan <108383190+regii17@users.noreply.github.com>
2023-11-01 00:56:32 +0700

    Add files via upload

---
+++ DDP.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	
+
+		int b=1;
+		for(b=1;b<=7;b++){
+		if (b%2==0){
+				cout<<"__";
+				
+			}
+			else{
+			cout<<" ";
+		}
+		}
+		cout<<endl;	
+	for(int d=1;d<=3;d++){
+	
+		int c=1;
+		for(c=1;c<=7;c++){
+			
+			if (c%2==0){
+				cout<<"__";
+				
+			}
+			else{
+			cout<<"|";
+		}
+			
+		}
+		cout<<endl;	
+	
+}
+	
+
+}
+
+
+
